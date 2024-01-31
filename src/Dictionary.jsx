4bfc11a5f8dddad71ba81273f76353e352eab064
Hayley <37635523+greenough-h@users.noreply.github.com>
2024-01-31 15:42:06 -0800

    Shecodes dictionary api integration

--- src/Dictionary.jsx
+++ src/Dictionary.jsx
@@ -1,11 +1,19 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 export default function Dictionary() {
   let [keyWord, setKeyWord] = useState(null);
 
+  function handleResponse(response) {
+    console.log(response.data.word);
+  }
+
   function searchWord(event) {
     event.preventDefault();
-    alert(`searching for ${keyWord}`);
+    let searchedWord = keyWord;
+    const apiKey = "t0cee57ed010o387a24333e4fba6d54e";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${searchedWord}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function updateWord(event) {
