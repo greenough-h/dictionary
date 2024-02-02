841b9eddc1b280b94ab1d992aa5718ae9d66cc92
Hayley <37635523+greenough-h@users.noreply.github.com>
2024-02-02 13:02:54 -0800

    Add default loading page

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App container">
       <h1>Dictionary</h1>
-      <Dictionary />
+      <Dictionary defaultKeyWord="sunflower" />
       <footer>
         <p>
           Coded by{" "}
--- src/Dictionary.jsx
+++ src/Dictionary.jsx
@@ -2,33 +2,46 @@ import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
 
-export default function Dictionary() {
-  let [keyWord, setKeyWord] = useState(null);
+export default function Dictionary(props) {
+  let [keyWord, setKeyWord] = useState(props.defaultKeyWord);
   let [results, setResults] = useState(null);
+  let [loaded, setLoaded] = useState(false);
 
   function handleResponse(response) {
     setResults(response.data);
   }
 
+  function search() {
+    const apiKey = "t0cee57ed010o387a24333e4fba6d54e";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyWord}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
   function searchWord(event) {
     event.preventDefault();
-    let searchedWord = keyWord;
-    const apiKey = "t0cee57ed010o387a24333e4fba6d54e";
-    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${searchedWord}&key=${apiKey}`;
-    axios.get(apiUrl).then(handleResponse);
+    search();
   }
 
   function updateWord(event) {
     setKeyWord(event.target.value);
   }
 
-  return (
-    <div className="container Dictionary">
-      <form onSubmit={searchWord}>
-        <input type="search" onChange={updateWord} />
-        <input type="submit" value="Search" />
-      </form>
-      <Results results={results} />
-    </div>
-  );
+  function load() {
+    setLoaded(true);
+    search();
+  }
+
+  if (loaded) {
+    return (
+      <div className="container Dictionary">
+        <form onSubmit={searchWord}>
+          <input type="search" onChange={updateWord} />
+          <input type="submit" value="Search" />
+        </form>
+        <Results results={results} />
+      </div>
+    );
+  } else {
+    load();
+    return "Loading...";
+  }
 }
